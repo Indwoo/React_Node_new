594aec8e9e96a951517c9f2abccb34952fb56d37
Indu <wjsdlsdn1158@naver.com>
2024-04-15 20:36:18 +0900

    git init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Indwoo:0504@cluster0.5zredre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+{}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
